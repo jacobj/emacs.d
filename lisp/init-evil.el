504d3807949c716912f1c5d6424e87ef96267aee
Jacob Jenkins <jacob@terainsights.com>
2015-07-13 11:02:07 -0400

    Update evil settings, add linux settings

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -10,4 +10,7 @@
 ;; Stop Evil from being evil and messing with my cursor
 (setq evil-default-cursor t)
 
+;; Quick dired jump.
+(define-key evil-normal-state-map (kbd "-") 'dired-jump)
+
 (provide 'init-evil)
---
+++ lisp/linux.el
@@ -0,0 +1,7 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Linux specific config ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(set-face-attribute 'default nil :font "Meslo LG S" :width 'normal :weight 'normal :height 105)
+
+(provide 'linux)
