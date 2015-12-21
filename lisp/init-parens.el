7b955c1d33a10f9e1ac821d411ec2279a6f49203
Jacob Jenkins <jacob@terainsights.com>
2015-12-21 11:06:46 -0500

    Add beautiful rainbow parens

---
+++ lisp/init-parens.el
@@ -0,0 +1,4 @@
+;; RAINBOWS
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+(provide 'init-parens)
--- lisp/linux.el
+++ lisp/linux.el
@@ -2,6 +2,9 @@
 ;; Linux specific config ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Inherit path from shell
+(exec-path-from-shell-initialize)
+
 (set-face-attribute 'default nil :font "Hack" :width 'normal :height 105)
 
 (provide 'linux)
