ab57c9bb41535fd3d7a961f122c5921fe1c44d19
Jacob <jacob@terainsights.com>
2016-01-28 14:28:41 -0500

    Small changes

--- lisp/linux.el
+++ lisp/linux.el
@@ -5,6 +5,8 @@
 ;; Inherit path from shell
 (exec-path-from-shell-initialize)
 
+(setenv "GRADLE_OPTS" "-Dorg.gradle.native=false")
+
 (set-face-attribute 'default nil :font "Hack" :width 'normal :height 105)
 
 (provide 'linux)
